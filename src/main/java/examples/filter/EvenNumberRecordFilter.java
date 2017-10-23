76748a153b9a975cc1aad92d9df1b65c39022bbb
Crim <stephen.powis@gmail.com>
2017-10-24 07:40:56 +0900

    initial commit

---
+++ src/main/java/examples/filter/EvenNumberRecordFilter.java
@@ -0,0 +1,20 @@
+package examples.filter;
+
+import com.darksci.kafka.webview.plugin.filter.RecordFilter;
+
+import java.util.Map;
+
+/**
+ * Example filter that filters even numbered offsets.
+ */
+public class EvenOffsetFilter implements RecordFilter {
+    @Override
+    public boolean filter(final String topic, final int partition, final long offset, final Object key, final Object value) {
+        return (offset % 2 == 0);
+    }
+
+    @Override
+    public void configure(final Map<String, ?> configs) {
+
+    }
+}
